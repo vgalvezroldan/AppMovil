a32dc8c968b79e03860bc354c02425de5a8b3688
vgalvezroldan <vic.galvez@duocuc.cl>
2023-10-22 15:12:32 -0300

    Cambios 22-10

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { NavController } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http'; // No olvides importar HttpClient
+
 
 @Component({
   selector: 'app-welcome',
@@ -14,32 +14,15 @@ export class WelcomePage implements OnInit {
   weatherData: any; // Propiedad para almacenar los datos del clima
   currentUser: string | null = null; // Propiedad para almacenar el nombre del usuario
 
-  constructor(
-    private authService: AuthService, 
-    private navCtrl: NavController,
-    private http: HttpClient // Inyecta HttpClient
-  ) { }
+  constructor(private authService: AuthService, private navCtrl : NavController) { }
 
   ngOnInit() {
     this.currentUser = this.authService.getCurrentUser(); // Obteniendo el usuario actual desde el AuthService
-    this.loadWeather(); // Cargar el clima cuando se inicialice el componente
   }
 
-  loadWeather() {
-    // Usaremos la API de MetaWeather como ejemplo, ya que no requiere una clave API.
-    const apiUrl = 'https://www.metaweather.com/api/location/44418/'; // Esta URL es para el pronóstico de Londres
-
-    this.http.get(apiUrl).subscribe(data => {
-      this.weatherData = data;
-    }, error => {
-      console.error('Error al obtener los datos del clima', error);
-    });
-  }
-
-  volver() {
+  volver = () => {
     this.navCtrl.navigateBack('/login');
-  }
 }
-
+}
 
 
